15665241c74e41f4adf7fb5d0613cccf7607c06b
Zach <G00304168@stu.raritanval.edu>
2022-12-09 00:07:06 -0500

    mouse game complete

--- Assets/RW/C/UIManagerScript1.cs
+++ Assets/RW/C/UIManagerScript1.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UIManagerScript1 : MonoBehaviour
 {
+public Animator startButton;
+public Animator settingsButton;
+public Animator dialog;
+public Animator contentPanel;
+public Animator gearImage;
+public void CloseSettings() 
+{
+    startButton.SetBool("isHidden", false);
+    settingsButton.SetBool("isHidden", false);
+    dialog.SetBool("isHidden", true);
+}
 public void StartGame() 
 {
     SceneManager.LoadScene("RocketMouse");
 }
+public void OpenSettings() 
+{
+    startButton.SetBool("isHidden", true);
+    settingsButton.SetBool("isHidden", true);
+    dialog.SetBool("isHidden", false);
 }
-
-
-
-
+public void ToggleMenu() 
+{
+    bool isHidden = contentPanel.GetBool("isHidden");
+    contentPanel.SetBool("isHidden", !isHidden);
+    gearImage.SetBool("isHidden", !isHidden);
+}
+}
 
